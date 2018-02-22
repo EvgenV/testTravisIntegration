d43cb1603316be7d1f6297bee9a30303f062bd9d
IEvgen Verkush <evgen.verkush@gmail.com>
2018-02-22 14:33:38 +0200

    - set manual codesigning - fix travis congif

--- testTravisIntegrationTests/testTravisIntegrationTests.swift
+++ testTravisIntegrationTests/testTravisIntegrationTests.swift
@@ -22,6 +22,12 @@ class testTravisIntegrationTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(true)
+    }
+    
+    func testExample1() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
         XCTAssert(false)
     }
     
